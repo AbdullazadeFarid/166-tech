76358257a5eba6d95d8e143c0d72a0d7b3e31e91
AbdullazadeFarid <abdullazadefarid@mail.ru>
2024-09-11 21:12:34 +0300

    .

---
+++ src/component/slider/slider.jsx
@@ -0,0 +1,133 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/swiper-bundle.css';
+import user from '../../assets/image/user.png'
+import starImg  from '../../assets/icon/Star.svg'
+
+import 'swiper/css';
+import "./slider.css"
+
+
+import React from 'react'
+
+const Homeswiper = () => {
+  return (
+
+    <>
+
+<Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+
+      spaceBetween={50}
+      slidesPerView={3}
+      navigation
+
+      onSlideChange={() => console.log('slide change')}
+      breakpoints={{
+        230: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        1440: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+      }}
+    >
+      <SwiperSlide>
+        <div className="home_slider_comment_cont">
+            <div className='home_slider_comment_cont_head'>
+              <img src={user} alt="" />
+              <p>01</p>
+            </div>
+              <img className='home_slider_comment_cont_stars' src={starImg} alt="" />
+              <p className='home_slider_comment_cont_desc'>Lorem ipsum dolor sit amet consectetur. Est vestibulum ornare viverra vestibulum neque turpis. Enim at amet et vivamus rhoncus.</p>
+              <div className='home_slider_comment_cont_line'></div>
+              <h2>Lisa Smith</h2>
+          </div>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <div className="home_slider_comment_cont">
+            <div className='home_slider_comment_cont_head'>
+              <img src={user} alt="" />
+              <p>01</p>
+            </div>
+              <img className='home_slider_comment_cont_stars' src={starImg} alt="" />
+              <p className='home_slider_comment_cont_desc'>Lorem ipsum dolor sit amet consectetur. Est vestibulum ornare viverra vestibulum neque turpis. Enim at amet et vivamus rhoncus.</p>
+              <div className='home_slider_comment_cont_line'></div>
+              <h2>Lisa Smith</h2>
+          </div>
+      </SwiperSlide>
+
+
+      <SwiperSlide>
+        <div className="home_slider_comment_cont">
+            <div className='home_slider_comment_cont_head'>
+              <img src={user} alt="" />
+              <p>01</p>
+            </div>
+              <img className='home_slider_comment_cont_stars' src={starImg} alt="" />
+              <p className='home_slider_comment_cont_desc'>Lorem ipsum dolor sit amet consectetur. Est vestibulum ornare viverra vestibulum neque turpis. Enim at amet et vivamus rhoncus.</p>
+              <div className='home_slider_comment_cont_line'></div>
+              <h2>Lisa Smith</h2>
+          </div>
+      </SwiperSlide>
+
+
+      <SwiperSlide>
+        <div className="home_slider_comment_cont">
+            <div className='home_slider_comment_cont_head'>
+              <img src={user} alt="" />
+              <p>01</p>
+            </div>
+              <img className='home_slider_comment_cont_stars' src={starImg} alt="" />
+              <p className='home_slider_comment_cont_desc'>Lorem ipsum dolor sit amet consectetur. Est vestibulum ornare viverra vestibulum neque turpis. Enim at amet et vivamus rhoncus.</p>
+              <div className='home_slider_comment_cont_line'></div>
+              <h2>Lisa Smith</h2>
+          </div>
+      </SwiperSlide>
+
+
+      <SwiperSlide>
+        <div className="home_slider_comment_cont">
+            <div className='home_slider_comment_cont_head'>
+              <img src={user} alt="" />
+              <p>01</p>
+            </div>
+              <img className='home_slider_comment_cont_stars' src={starImg} alt="" />
+              <p className='home_slider_comment_cont_desc'>Lorem ipsum dolor sit amet consectetur. Est vestibulum ornare viverra vestibulum neque turpis. Enim at amet et vivamus rhoncus.</p>
+              <div className='home_slider_comment_cont_line'></div>
+              <h2>Lisa Smith</h2>
+          </div>
+      </SwiperSlide>
+
+
+            <SwiperSlide>
+        <div className="home_slider_comment_cont">
+            <div className='home_slider_comment_cont_head'>
+              <img src={user} alt="" />
+              <p>01</p>
+            </div>
+              <img className='home_slider_comment_cont_stars' src={starImg} alt="" />
+              <p className='home_slider_comment_cont_desc'>Lorem ipsum dolor sit amet consectetur. Est vestibulum ornare viverra vestibulum neque turpis. Enim at amet et vivamus rhoncus.</p>
+              <div className='home_slider_comment_cont_line'></div>
+              <h2>Lisa Smith</h2>
+          </div>
+      </SwiperSlide>
+
+    </Swiper>
+
+    </>
+  )
+}
+
+export default Homeswiper
